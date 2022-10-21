1e1f8f2f0bd61a41dc54214f59a750c81a6984f8
Leva <piknik0711@yandex.ru>
2022-10-21 15:12:28 +0300

    Initial commit

---
+++ src/main/java/com/arcadag/samsolka/SamsOlkaApplication.java
@@ -0,0 +1,13 @@
+package com.arcadag.samsolka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamsOlkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamsOlkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arcadag/samsolka/SamsOlkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcadag.samsolka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamsOlkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
